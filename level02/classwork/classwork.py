b8561ebf0878091d23f76533a109deca9bf7a7ca
nu0ca <nucadigmelashvili22@gmail.com>
2025-07-07 11:40:41 -0700

    GOA

---
+++ level02/classwork/classwork.py
@@ -0,0 +1,13 @@
+num1=40
+num2=50
+print(num1 + num2)
+
+
+
+num1=30
+num2=25
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
---
+++ level02/homework/homework.py
@@ -0,0 +1,68 @@
+#davaleba 1
+num1=50
+num2=10
+
+print(50-10)
+print(50+10)
+print(50*10)
+print(50/10) 
+
+
+#davaleba 2
+name1="aleqsandre"
+name2="giorgi"
+name3="mari"
+name4="ana"
+
+num1=10
+num2=40
+num3=35
+num4=50
+
+num5=10,5
+num6=5,5
+num7=3,3
+num8=29,8
+
+print(name1)
+print(name2)
+print(name3)
+print(name4)
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+print(num6)
+print(num7)
+print(num8)
+
+#davaleba 3
+name1="mariami"
+name2="ana"
+print("mariami"+"ana")
+#სტრინგების ერთმანეთთან დაამატებას ეწოდება კომკატიანცია.
+
+#davaleba 4
+name1="nini"
+name2="ana"
+name3="mariami"
+name4="saba"
+name5="tornike"
+
+print(name1+name2+name3+name4+name5)
+
+#davaleba 5
+
+num1=10
+num2=0.5
+num3=10,5
+num4=5
+num5=9,5
+
+#num3
+print(10+0.5)
+#num4
+print(10*0.5)
+#num5
+print(10-0.5)
