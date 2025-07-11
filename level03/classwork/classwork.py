5d7393468e03c53da5cd4b17eef4f4b464088f32
nu0ca <nucadigmelashvili22@gmail.com>
2025-07-11 05:33:08 -0700

    GOA

---
+++ level03/classwork/classwork.py
@@ -0,0 +1,56 @@
+# davaleba1
+#ვწერთ ცვლადს,შემდეგ ვანახლებთ მას და ვპრინტავთ ისე
+name="mari"
+name="ani"
+print(name)
+
+#davaleba 2
+
+name="gio"
+Name="ana"
+NAme="nino"
+NAMe="lizi"
+NAME="sandro"
+
+# დავალება 3
+#ვწერთ ოთხ ცვლადს სადაც ვინახავთ string ტიპის მონაცემებს
+user_name="digmelashvili"
+user_name1="bolqvadze"
+user_name2="baramashvili"
+user_name3="abesadze"
+
+print("digmelashvili")
+print("bolqvadze")
+print("baramashvili")
+print("abesadze")
+
+
+#vwer otx cvlads sadac vinaxav integer tipis monacemebs
+
+kilo_grams=15
+kilo_grams1=20
+kilo_grams2=29
+kilo_grams3=56
+
+print(15)
+print(20)
+print(29)
+print(56)
+
+#vwert otx cvlads sadac vinaxavt float tipis monacems
+
+kilo_grams4=9,6
+kilo_grams5=10,5
+kilo_grams6=11,5
+kilo_grams7=24,7
+
+print(9,6)
+print(10,5)
+print(11,5)
+print(24,7)
+
+#davaleba 2
+user_name="goga"
+user_surname="chalauri"
+first_number=200
+second_number=123
---
+++ level03/homework/homework.py
@@ -0,0 +1,64 @@
+#დავალება 1
+#ვქმნით ორ ცვლადს snake_case გამოყენებით,რომელზედაც მოვახდენთ მათემათიკურ მოკმედებებს(+,-,*,/).
+num_1=20
+num_2=15
+sum_of_numbers=num_1+num_2
+number_abbreviation=num_1-num_2
+multyipling_numbers=num_1*num_2
+dividing_numbers=num_1/num_2
+
+#დავალება 2
+#ვქმნით ცვლადს და ვანიჭებთ მნიშვნელობას,შემდეგ მას ვანახლებთ ხუთჯერ და ვპრინტავთ საბოლოო მნიშვნელობას
+name="nuca"
+name="irakli"
+name="mari" 
+name="gio"
+name="sandro"
+name="saba"
+print(name)
+
+#დავალება 3
+#ვქმნით 5 ცვლადს რომელსაც ერქმევა ერტი და იგივე სახელები,მაგრამ იქნება case sensitive ენა,ხუთივე მათგანს ვპრინტავთ ცალცალკე
+name="ana"
+Name="nini"
+NAme="nika"
+NAMe="lasha"
+NAME="goga"
+print(name)
+print(Name)
+print(NAme)
+print(NAMe)
+print(NAME)
+
+#დავალება 4
+#1 2name="giorgi"
+#2 user{name="bubunauri"
+#3 user_name=goga 
+#4 user-surname=axalaia
+
+#1
+name2="giorgi"
+#2
+user_name="bubunauri"
+#3
+user_name="gogo"
+#4
+user_surname="axalaia"
+
+#დავალება 5
+#ვქმნით ხუთ ცვლადს რომელსაც მივანიჭებთ სტრინგ ტიპის მონაცემენს შემდეგ კი დავუმატებთ ერთმანეთს
+user_name="babunadze"
+user_name="gocholeishvili"
+user_name="digmelashvili"
+user_name="babenkovi"
+user_name="kevlishvili"
+print("babunadze"+"gocholeishvili"+"digmelashvili"+"babenkovi"+"kevlishvili")
+#სტრინგების ერთმანეთთან დამატებას ეწოდება კონკატინაცია
+
+#დავალება 6
+name="nuca"
+num=10
+print("nuca"*10)
+
+#davaleba 7
+#სტრინგის და ინტეჯერის დამატება,გამოკლება და გაყოფა არ შეგვიძლია.თუ მათზე ამ მათემათიკურ მოქმედებებს მოვახდენთ ტერმინალში გამოიტანს ერორს შეცხომას მასზე მხოლოდ შგვიძლია მოვახდინოთ გამრავლება
