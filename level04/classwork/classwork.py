a3806fdeb71f0822b65a3f295156cd8633815c59
nu0ca <nucadigmelashvili22@gmail.com>
2025-07-15 07:02:37 -0700

    GOA

---
+++ level04/classwork/classwork.py
@@ -0,0 +1,23 @@
+#davaleba 1
+name=input("enter your name :")
+print(name)
+
+#davaleba 2
+num1=input("what is your age")
+num2=input("what is your friends age ")
+print(int(num1) + int(num2))
+
+#davaleba 3
+age=input("what is your age:")
+print(type(age))
+
+#davaleba 4
+name1=("nuca")
+name2=15
+name3=0,5
+name4=True
+
+print(type(name1))
+print(type(name2))
+print(type(name3))
+print(type(name4))
