2c4f5ef50206fcffed7e5e8c78308463ae603ecb
nu0ca <nucadigmelashvili22@gmail.com>
2025-07-22 07:18:26 -0700

    GOA

---
+++ level06/classwork/classwork.py
@@ -0,0 +1,19 @@
+#davaleba 1
+#კომენტარების სახით ახსენით რა არის logical operators (and, or), რისთვის ვიყენებთ და მოიყვანეთ შესაბამისი მაგალითები.
+#logical operators_ (and; or)
+#and_ოპერატორის გამოყენების დროს თუ ერთი პირობა მაინც არის მცდარი,მაშინყველა პირობა არის მცდარი
+#true and true -->true
+#true and false -->false
+#ორ_ოპერატორის გამოყენების დრპს თუ ერთი პირობა მაინც არის სწორი მაშინ სუყველა პირობა არის სწორი.
+#true or true -->true
+#true or false -->true
+
+#davaleba2
+ #მომხმარეებელს შემოაყვანინეთ სახელი, შეამოწმეთ იგი თუ უდრის თქვენს სახელს, არ დაგავიწყდეთ print ფუნქციის გამოყენება პასუხის დასაბეჭდად.
+name="ana" 
+print(name+"nuca")
+
+#davaleba 3
+# გააკეთეთ 2-2 მაგალითი or და and ოპერატორებზე
+#true or false -->true     false or false-->false
+#true and false-->false     true and true-->true
