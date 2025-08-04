245741dbdf1bac3ae12a31987c709d8b27657407
nu0ca <nucadigmelashvili22@gmail.com>
2025-08-04 02:58:16 -0700

    GOA

---
+++ level10/classwork/claswork.py
@@ -0,0 +1,10 @@
+#davaleba 1
+#1)დაატრიალეთ for loop 50 დან 100 მდე და გამოიტანეთ ყველა რიცხვი ტერმინალში
+for i in range(50,100):
+    print(i)
+
+#davaleba2
+#დაატრიალეთ for loop 3 დან 76 მდე
+for i in range(3,76):
+    print(i)
+    
---
+++ level10/homework/homework.py
@@ -0,0 +1,74 @@
+#davaleba1
+#დაატრიალეთ for ციკლი 0 დან 38 ის ჩათვლით და გამოიტანე თითოეული რიცხვი ტერმინალში 
+for i in range(0,39):
+    print(i)
+
+#davaleba2
+#დაატრილეთ for ციკლი 30 დან 40 მდე  და გამოიტანე თითოეული რიცხვი ტერმინალში 
+for i in range(30,40):
+     print(i)
+
+#davaleba3
+#დაატრიალეთ for ციკლი 12 იდან 120 მდე  და გამოიტანე თითოეული რიცხვი ტერმინალში 
+for i in range(12,120):
+     print(i)
+
+#davaleba4
+#დაატრიალეთ for ციკლი და გამოიტანეთ თქვენი სახელი 18 ჯერ ასევე გამოიტანეთ თქვენი ასაკიც,ერთ for ციკლში მოათავსეთ ორივე და ეგრე გააკეთეთ
+for i in range(18):
+     name="nuca"
+     print(name)
+     age=15
+     print(age)
+
+#davaleba5
+#დაატრიალეთ for ციკლი 10 დან 100 მდე 3 step ის გამოტოვებით და გამოიტანეთ თითოეული რიცხვი ტერმინალში
+for i in range(10,100,3):
+     print(i)
+
+#davaleba6
+#ტერმინალში გამოიტანეთ თქვენი საყვარელი ხილის სახელი 30 ჯერ და თან დანომრეთ ,მიიღეთ ქვემოთ მოცემულის ნაირი შედეგი
+for i in range(30):
+     print(str(i)+"marwyvi")
+
+#davaleba7
+#მომხმარებელს შემოატანინეთ მისი ასაკი 13 ჯერ გამოიყენე for loop
+for i in range(13):
+     age=input("enter your age")
+     print(age)
+
+#davaleba8
+#გამოიტანეთ თქვენი სახელი 10 ჯერ while loop ის გამოყენებით
+i=0
+while i<10:
+    print("nuca")
+    i=i+1
+
+#davaleba9
+#დაითვალეთ 0 დან 24 მდე გამოიყენეთ while loop
+i=0
+while i<24:
+     print(i)
+     i=i+4
+
+#davaleba10
+#იმუშაოვეთ ბევრი for და while loop ებზე მოიყვანეთ თქვენით მაგალითები სურვილისამებრ
+#1 for loop
+for i in range(3):
+     age=input("enter your age")
+     print(age)
+#2 for loop
+for i in range(30):
+     print(i)
+
+#1 while loop
+i=0
+while i<5:
+     print("qatami")
+     i=i+1
+
+#2while loop
+i=0
+while i<20:
+     print(i)
+     i=i+5
