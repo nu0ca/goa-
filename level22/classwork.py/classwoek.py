5134af1d486b47d403bb926030aa3948c316739a
nu0ca <nucadigmelashvili22@gmail.com>
2025-09-14 05:02:39 -0700

    1

---
+++ level22/classwork.py/classwoek.py
@@ -0,0 +1,24 @@
+#davaleba1
+#შექმენით სია სადაც გექნებათ მოთავსებული მხოლოდ და მხოლოდ სტრინგ ტიპის მონაცემები(სიაში შეიყვანეთ 10 სახელი),თქვენი დავალებაა ამ სიიდან გამოიტანოთ თითეული სახელი ოღნდ ცალ ცალკე,გამოიოყენეთ ინდექსინგი
+box=["nika","luka" ,"mari","nini" ,"goga","giga ","taso" ,"ana" ,"gio" ,"lizi"]
+print(box[0])
+print(box[1])
+print(box[2])
+print(box[3])
+print(box[4])
+print(box[5])
+print(box[6])
+print(box[7])
+print(box[8])
+print(box[9])
+#amisi varianti 2
+for i in range(0,7):
+    print(box[i])
+
+#davaleba2
+# შექმენით ცვლადი სადაც შეინახავთ ბოსტნეულის სახელებს,უნდა გქონდეთ სულ 6 ბოსტნეულის სახელი,თქვენი დავალებაა რომ ამ სიაში მყოფი მე 0 ე ინდექსზე მდგომი ელემენტი ჩაანაცვლოთ "viri" ით ,ამ სიაშ მყოფი მესამე დ=ინდექსზე მდგომი ელემენტი ჩაანაცვლოთ "tagvit" და ამ სიაში მყოფი მეხუთე ინდექსზე მდგომი ელემენტი ჩაანაცვლოთ "bokveri" ით, როდესაც შეცვლი სამივე მნიშვნელობას დაპრინტე საბოლოო განახლებული სია
+vegetables=["marwyvi","kartopili","atami","vashli","legvi","kitri"]
+vegetables[0]="viri"
+vegetables[3]="tagvi"
+vegetables[5]="bokveri"
+print(vegetables)
