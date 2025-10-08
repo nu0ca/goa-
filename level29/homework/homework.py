a7c335b0d3029eaaf1052f4c34fe9a19327a46c6
nu0ca <nucadigmelashvili22@gmail.com>
2025-10-08 12:43:19 -0700

    1

---
+++ level29/homework/homework.py
@@ -0,0 +1,53 @@
+#davaleba1
+#მოცემულია სტრინგი "Programming".
+# ამოიღე პირველი 6 სიმბოლო და დაბეჭდე გამოიყენეთ slicing
+name="Programming"
+print(name[0:6])
+
+#davaleba2
+#მოცემულია სია numbers = [10, 20, 30, 40, 50, 60, 70].
+# ამოიღე მხოლოდ შუა 3 ელემენტი და დაბეჭდე გამოიყენეთ slicing (მინუს ინდექსებითაც)
+numbers = [10, 20, 30, 40, 50, 60, 70]
+print(numbers[2:5])
+print(numbers[-3:-5])
+
+#davaleba3
+#მოცემულია სტრინგი "HelloWorld".
+# დაბეჭდეთ Hello ტერმინალში slicing ის გამოყენებით (მინუს ინდექსებითაც)\
+nam="HelloWorld"
+print(nam[0:5])
+print(nam[:-5])
+
+#davaleba4
+#მოცემულია სია letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'].
+# დაბეჭდე ყოველ პირველი მესამე მეხუთე ელემენტები გამოიყენეთ indexing (მინუს ინდექსებითაც)
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters[0])
+print(letters[2])
+print(letters[4])
+
+print(letters[-3])
+print(letters[-5])
+print(letters[-7])
+
+#davaleba5
+#მოცემულია სტრინგი "Information".
+# ამოიღე "forma" ნაწყვეტი slicing-ით (მინუს ინდექსებითაც)
+fr="Information"
+print(fr[2:7])
+print(fr[-9:-4])
+
+#davaleba6
+#მოცემულია სტრინგი "abcdefghijklmno".
+# შექმენი სამი სხვადასხვა სლაისი:
+
+# პირველი შეიცავდეს მხოლოდ a დან d მდე ასოებს
+
+# მეორე – შეიცავდეს j დან o მდე ასოებს
+
+# მესამე – შეიცავდეს f დან j მდე ასოებს
+nav="abcdefghijklmno"
+print(nav[0:3])
+print(nav[9:13])
+print(nav[5:8])
+
