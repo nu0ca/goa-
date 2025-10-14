31c80c12149d0e48eba6b93ee7794868e522491f
nu0ca <nucadigmelashvili22@gmail.com>
2025-10-14 11:56:02 -0700

    1

---
+++ level31/classwork/classwork.py
@@ -0,0 +1,6 @@
+#davaleba1
+#შექმენით სია -->  ["ina" , "givi" , "nika" , "daviti" , "ia" , "lizi"] , თქვენი დავალებაა რომ პირველი 2 ელემენტი ჩაანაცვლოთ შემდეგი სიით --> ["irina" , "milana" , "kira", "mate"] //////////////// და ასევე სიის ბოლო ორი ელემენტი შეანაცვლე შემდეგი სიით --> ["gia" , "emzari" , "xvicha"] ამის შემდეგ დაპრინტეთ საბოლოო სია
+list =["ina" , "givi" , "nika" , "daviti" , "ia" , "lizi"]
+list[0:2]=["irina" , "milana" , "kira", "mate"]
+list[4:]=["gia" , "emzari" , "xvicha"] 
+print(list)
